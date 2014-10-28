0a567ee14b4f1aaf27e056d1e0db58c6db0da80a
anykey84 <kalimulin-ru@yandex.ru>
2014-10-28 07:42:16 +0300

    tes errors! Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,11 +24,11 @@ module SessionsHelper
     user == current_user
   end
 
-  def sign_out
-    current_user.update_attribute(:remember_token,
-                                  User.encrypt(User.new_remember_token))
-    cookies.delete(:remember_token)
-    self.current_user = nil
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
 
   def redirect_back_or(default)
@@ -38,5 +38,10 @@ module SessionsHelper
 
   def store_location
     session[:return_to] = request.url if request.get?
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 end
